d07197cbc61dc3bdf165de56eb9555973eb3e59e
David Wilkins <david@wilkox.org>
2011-08-30 15:58:37 +1000

    Added etymology parser

--- parse-etymology.pl
+++ parse-etymology.pl
@@ -1,7 +1,44 @@
 #!/usr/bin/perl
+use warnings;
 
 #Parses the dictionary into a simple list of
 # words of latin, french and german/old english
 # origins
 
+%lang = (
+	'OE' => 'old english',
+	'F' => 'french',
+	'L' => 'latin',
+	'LL' => 'late latin',
+	'AS' => 'anglo-saxon',
+);
 
+%fancy = (
+	'OE' => 'not fancy',
+	'AS' => 'not fancy',
+	'F' => 'fancy',
+	'L' => 'fancy',
+	'LL' => 'fancy',
+);
+
+die unless open(DICT, "<dict/pgwht04.txt");
+die unless open(OUT, ">dict/ety.csv");
+
+my $word;
+my $ety;
+my $lang;
+while (my $line = <DICT>) {
+	if ($line =~ /<h1>(.+)<\/h1>/) {
+		$word = $1;
+	}		
+	if ($line =~ /<ety>(.+)<\/ety>/) {
+		$ety = $1;
+		$ety =~ /\[([^\.]+)\./;
+#		print "\nlang is $1";
+		next unless exists $lang{$1};
+		print OUT "$word, $1\n";				
+	}
+}
+
+close OUT;
+close DICT;
