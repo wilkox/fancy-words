be70cfb2e4a133b67be4907d9f5100a1c8f7e3ce
David Wilkins <david@wilkox.org>
2011-08-30 15:37:01 +1000

    Added README and started dictionary

---
+++ README.markdown
@@ -0,0 +1,3 @@
+
+##Dictionary
+The [Project Gutenberg Webster's Unabridged Dictionary](http://www.gutenberg.org/dirs/etext96/pgwht04.txt)
---
+++ parse-etymology.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+#Parses the dictionary into a simple list of
+# words of latin, french and german/old english
+# origins
+
+
